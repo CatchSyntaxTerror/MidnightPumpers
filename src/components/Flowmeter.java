b68b9136baa5effcf666fd45478735bb1a74c0fb
Daniel Thompson <dthompson1@unm.edu>
2025-08-31 10:18:02 -0600

    flowmeter visuals

--- src/components/Flowmeter.java
+++ src/components/Flowmeter.java
@@ -1,14 +1,29 @@
 package components;
 
 import javafx.scene.text.Text;
+import sim.Gas;
 
 public class Flowmeter {
     private Text text;
+    private Gas gas;
+    private int gasFlow;
     public Flowmeter(){
         this.text = new Text("0000");
+        this.gas = null;
+        this.gasFlow = 0;
     }
 
     public void setText(Text text) {
         this.text = text;
+    }
+    public void checkingFlow(){
+        this.gasFlow += 1;
+        updatedText();
+    }
+    private void updatedText(){
+        this.text.setText(String.valueOf(this.gasFlow));
+    }
+    public void setGas(Gas gas) {
+        this.gas = gas;
     }
 }
--- src/components/Pump.java
+++ src/components/Pump.java
@@ -1,14 +1,20 @@
 package components;
 
 import javafx.scene.shape.Rectangle;
+import sim.Gas;
 
 public class Pump {
     private Rectangle rectangle;
+    private Gas gas;
     public Pump(){
+        this.gas = null;
         this.rectangle = null;
     }
 
     public void setRectangle(Rectangle rectangle) {
         this.rectangle = rectangle;
+    }
+    public void setGas(Gas gas) {
+        this.gas = gas;
     }
 }
