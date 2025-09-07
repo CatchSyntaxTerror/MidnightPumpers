70138ccb27673f1c13c4f501e827d3f78755a4e0
CatchSyntaxTerror <youssefaminbeh@gmail.com>
2025-09-07 13:07:01 -0600

    started gas station gui

---
+++ src/UI/GasStationGUI.java
@@ -0,0 +1,45 @@
+package UI;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+import java.awt.*;
+
+public class GasStationGUI extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("GasStation");
+        stage.setResizable(false);
+
+        Image bgImage = new Image("/cartoon-gas-station-background.jpg");
+        BackgroundImage backgroundImage =
+                new BackgroundImage(
+                        bgImage,
+                        BackgroundRepeat.NO_REPEAT,
+                        BackgroundRepeat.NO_REPEAT,
+                        BackgroundPosition.CENTER,
+                new BackgroundSize(100, 100, true, true, true, false)
+                );
+        StackPane root = new StackPane();
+        root.setBackground(new Background(backgroundImage));
+
+//        Label label = new Label("Gas Prices");
+//        label.setStyle("-fx-background-color: black; -fx-text-fill: white;");
+//        label.setAlignment(Pos.CENTER);
+//        root.getChildren().add(label);
+        // TODO: add labels for gas prices
+        Scene scene = new Scene(root, 800, 380);
+        stage.setTitle("Background Image Example");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
